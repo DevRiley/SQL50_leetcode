2552160df01488ec9d437a86cc8afa48e9ad3cca
R0002l <95018177+R0002l@users.noreply.github.com>
2025-11-30 23:21:31 +0800

    Update Solution.sql

--- 1661. Average Time of Process per Machine/Solution.sql
+++ 1661. Average Time of Process per Machine/Solution.sql
@@ -4,5 +4,7 @@ SELECT
         SUM(CASE WHEN activity_type = 'end' THEN timestamp ELSE -timestamp END) 
         / COUNT(DISTINCT process_id)
     , 3) AS processing_time
-FROM Activity
-GROUP BY machine_id;
+FROM 
+    Activity
+GROUP BY 
+    machine_id;
